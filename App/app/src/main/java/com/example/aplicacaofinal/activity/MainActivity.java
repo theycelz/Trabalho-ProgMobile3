37906ba4fbbdef89ea5fc1460d2a0c8758fb7298
JerffersonJunior <jerfferson.junior@ufms.br>
2023-11-28 23:33:12 -0400

    feat: adiciona camera

--- App/app/src/main/java/com/example/aplicacaofinal/activity/MainActivity.java
+++ App/app/src/main/java/com/example/aplicacaofinal/activity/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.aplicacaofinal.activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.Toast;
 
@@ -109,5 +112,50 @@ public class MainActivity extends AppCompatActivity {
         botaoAcessar = findViewById(R.id.buttonAcesso);
         tipoAcesso = findViewById(R.id.switch1);
     }
+    public void tirarfoto(View view){
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data != null) {
+            Bundle bundle = data.getExtras();
+            if (bundle != null) {
+                Bitmap img = (Bitmap) bundle.get("data");
+                ImageView iv = (ImageView) findViewById(R.id.imageView);
+                iv.setImageBitmap(img);
+            }
+        }
+    }
+
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
