e50c3f2a253773438e56c757407904fca8e26854
JerffersonJunior <jerfferson.junior@ufms.br>
2023-11-29 04:49:33 -0400

    feat: coloca as cameras em funcionamento e muda textura de fundo

--- App/app/src/main/java/com/example/aplicacaofinal/activity/CadastrarObjetoActivity.java
+++ App/app/src/main/java/com/example/aplicacaofinal/activity/CadastrarObjetoActivity.java
@@ -1,16 +1,228 @@
 package com.example.aplicacaofinal.activity;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.aplicacaofinal.R;
+import com.example.aplicacaofinal.databinding.ActivityCadastrarObjetoBinding;
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
 
 public class CadastrarObjetoActivity extends AppCompatActivity {
+    private ActivityCadastrarObjetoBinding binding;
+    private static final int PERMISSION_REQUEST_CODE = 200;
+    static final int GALLERY = 2;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    byte foto[];
+    private static final String IMAGE_DIRECTORY = "/camera2022";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cadastrar_objeto);
+
+        binding = ActivityCadastrarObjetoBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnTirarFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Camera camera = Camera.open(1);
+                Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.putExtra("android.intent.extras.CAMERA_FACING", 1);
+                startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+            }
+        });
+
+        binding.btnCarregarFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, GALLERY);
+            }
+        });
     }
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (checkPermission()) {
+            // Success
+        } else {
+            requestPermission();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == this.RESULT_CANCELED) {
+            return;
+        }
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap bitmap = (Bitmap) extras.get("data");
+            binding.imageView7.setImageBitmap(resizeImage(bitmap,
+                    300, 600));
+            saveImage(bitmap);
+            Toast.makeText(CadastrarObjetoActivity.this, "Imagem Salva!",
+                    Toast.LENGTH_SHORT).show();
+        }else if (requestCode == GALLERY) {
+            if (data != null) {
+                Uri contentURI = data.getData();
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(
+                            this.getContentResolver(),
+                            contentURI);
+                    // Salvando a imagem
+                    String path = saveImage(bitmap);
+                    Log.i("TAG","Path: " + path);
+                    Toast.makeText(CadastrarObjetoActivity.this, "Imagem OK!",
+                            Toast.LENGTH_SHORT).show();
+                    // Print on screen
+                    binding.imageView7.setImageBitmap(resizeImage(bitmap, 300, 600));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(CadastrarObjetoActivity.this, "Falha!",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    public String saveImage(Bitmap bitmap) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream(
+                bitmap.getWidth() * bitmap.getHeight());
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        foto = bytes.toByteArray();
+
+        File directory = new File(Environment.getExternalStorageDirectory()
+                + IMAGE_DIRECTORY);
+        // Criando o diretório caso ele não exista!
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+        try {
+            File fileImage = new File(directory,
+                    Calendar.getInstance().getTimeInMillis() +".jpg");
+            fileImage.createNewFile();
+            FileOutputStream fo = new FileOutputStream(fileImage);
+            fo.write(bytes.toByteArray());
+            MediaScannerConnection.scanFile(this,
+                    new String[]{fileImage.getPath()},
+                    new String[]{"image/jpeg"}, null);
+            fo.close();
+            Log.d("TAG", "File Saved:->" + fileImage.getAbsolutePath());
+            return fileImage.getAbsolutePath();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        return "";
+    }
+
+    public static Bitmap resizeImage(Bitmap bitmap, int newWidth, int newHeight) {
+        Matrix m = new Matrix();
+        m.setScale((float) newWidth / bitmap.getWidth(), (float) newHeight / bitmap.getHeight());
+        //m.postRotate(90, 150, 300);
+        Bitmap output = Bitmap.createBitmap(newWidth, newHeight, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+        canvas.drawBitmap(bitmap, m, new Paint());
+        return output;
+    }
+    private boolean checkPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            return false;
+        }
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            return false;
+        }
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            return false;
+        }
+        return true;
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.CAMERA,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE},
+                PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getApplicationContext(), "Permission Granted",
+                            Toast.LENGTH_SHORT).show();
+                    // main logic
+                } else {
+                    Toast.makeText(getApplicationContext(), "Permission Denied",
+                            Toast.LENGTH_SHORT).show();
+                    binding.btnTirarFoto.setVisibility(View.INVISIBLE);
+                    binding.btnCarregarFoto.setVisibility(View.INVISIBLE);
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                            showMessageOKCancel("You need to allow access permissions",
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                requestPermission();
+                                            }
+                                        }
+                                    });
+                        }
+                    }
+                }
+                break;
+        }
+    }
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(CadastrarObjetoActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+
+
+
 }
--- App/app/src/main/java/com/example/aplicacaofinal/activity/MainActivity.java
+++ App/app/src/main/java/com/example/aplicacaofinal/activity/MainActivity.java
@@ -2,16 +2,12 @@ package com.example.aplicacaofinal.activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.graphics.Bitmap;
-
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.Toast;
 import com.example.aplicacaofinal.R;
@@ -117,23 +113,7 @@ public class MainActivity extends AppCompatActivity {
         botaoAcessar = findViewById(R.id.buttonAcesso);
         tipoAcesso = findViewById(R.id.switch1);
     }
-    public void tirarfoto(View view){
-        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-        startActivityForResult(intent, 0);
-    }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (data != null) {
-            Bundle bundle = data.getExtras();
-            if (bundle != null) {
-                Bitmap img = (Bitmap) bundle.get("data");
-                ImageView iv = (ImageView) findViewById(R.id.imageView);
-                iv.setImageBitmap(img);
-            }
-        }
-    }
 
 
 }
